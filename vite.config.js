4dc7487fd5a5d4cbfe16c8767f746f988b4e96b6
Dipak <dipakbohara710@gmail.com>
2024-09-08 23:41:12 +0400

    Deploy1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dip_portFolio/',
+  base: '/dipak_portFolio/',
   build: {
     assetsDir: 'assets' 
   },
